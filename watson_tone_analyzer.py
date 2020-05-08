1fe0fa530c086aa5a428dcb174da7c64f3e915a9
Velcorn <Velcorn@users.noreply.github.com>
2020-05-08 18:49:09 +0200

    Add files via upload

---
+++ watson_tone_analyzer.py
@@ -0,0 +1,42 @@
+import json
+from ibm_watson import ToneAnalyzerV3
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+# Set up Tone Analyzer and Translator from Watson
+api_key_ta = '1zk1LfROS3ccoX0iEOBomvK6euIpbJSkp9K-wu2IzS_A'
+api_key_tl = '2Q5-kJJN8MqVEgGxf5VM2Dv063cL7r5VTp44IcreG3EN'
+version = '2020-05-08'
+url_ta = 'https://api.eu-de.tone-analyzer.watson.cloud.ibm.com/instances/d0f39694-bd5f-4d32-900c-e689a9109a31'
+url_tl = 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/371a365a-dafb-4d40-8f7a-0da85693ee4a'
+
+authenticator_ta = IAMAuthenticator(api_key_ta)
+tone_analyzer = ToneAnalyzerV3(
+    version=version,
+    authenticator=authenticator_ta
+)
+
+authenticator_tl = IAMAuthenticator(api_key_tl)
+translator = LanguageTranslatorV3(
+    version=version,
+    authenticator=authenticator_tl
+)
+
+tone_analyzer.set_service_url(url_ta)
+translator.set_service_url(url_tl)
+
+# Get comment from text file.
+with open("comment.txt", "r") as f:
+    comment = f.read().replace("\n", " ")
+
+# Translate comment and analyze the tone.
+translation = translator.translate(comment, model_id='de-en').get_result()
+text = translation['translations'][0]['translation']
+tone_analysis = tone_analyzer.tone({'text': text}, content_type='application/json').get_result()
+
+# Output tone analysis to json file.
+with open("tone_analysis.json", "w", encoding="utf8") as f:
+    f.write("Input text: " + json.dumps(comment, ensure_ascii=False) + "\n")
+    f.write(json.dumps(tone_analysis, indent=2))
+
+print("Saved tone analysis to JSON")
