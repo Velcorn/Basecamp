20357fcf738e64713225fef4917419ff41d0d843
Velcorn <iwishyouperish@gmail.com>
2020-05-12 20:09:12 +0200

    Minor text files

--- flask_app.py
+++ flask_app.py
@@ -9,3 +9,7 @@ def index():
     with open("tone_analysis.txt", "r", encoding="utf8") as f:
         tone_analysis = f.read()
     return Response(tone_analysis, mimetype='text/plain')
+
+
+if __name__ == "__main__":
+    app.run()
