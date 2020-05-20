38bef6f00de07564c9e6bc804ab6b8570dc9d079
velcorn <iwishyouperish@gmail.com>
2020-05-21 01:14:12 +0200

    Fuck SSH!

--- webcrawl.py
+++ webcrawl.py
@@ -1,33 +1,30 @@
 import psycopg2
-from sshtunnel import SSHTunnelForwarder
+import paramiko
+import os
 from config import config
 
+ssh = paramiko.SSHClient()
+ssh.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+ssh.connect("rzssh1.informatik.uni-hamburg.de", username="6willrut", password="7TuB8binGL")
+print("SSH connected.")
+
 try:
-    with SSHTunnelForwarder(
-            ("rzssh1.informatik.uni-hamburg.de", 22),
-            ssh_username="6willrut",
-            ssh_password="7TuB8binGL",
-            remote_bind_address=("basecamp-bigdata.informatik.uni-hamburg.de", 5432)) as tunnel:
+    params = config()
+    conn = psycopg2.connect(**params)
+    cursor = conn.cursor()
+    print("Connected to DB.")
+    # Print PostgreSQL connection properties.
+    print(conn.get_dsn_parameters(), "\n")
 
-        tunnel.start()
-        print("SSH tunnel connected.")
-
-        params = config()
-        connection = psycopg2.connect(**params)
-        cursor = connection.cursor()
-        print("Connected to DB.")
-        # Print PostgreSQL Connection properties
-        print(connection.get_dsn_parameters(), "\n")
-
-        # Print PostgreSQL version
-        cursor.execute("SELECT version();")
-        record = cursor.fetchone()
-        print("You are connected to - ", record, "\n")
+    # Print PostgreSQL version.
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
 
 except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
 '''finally:
-    if connection:
+    if conn:
         cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed")'''
+        conn.close()
+        print("PostgreSQL connection is closed.")'''
