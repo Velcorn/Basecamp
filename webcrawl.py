b6faec21b0f8edf681c5f07d4ab66c5704a79568
Velcorn <iwishyouperish@gmail.com>
2020-05-20 18:15:08 +0200

    Postgres try #1

---
+++ webcrawl.py
@@ -0,0 +1,39 @@
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+
+try:
+    with SSHTunnelForwarder(
+            ("rzssh1.informatik.uni-hamburg.de", 22),
+            ssh_username="6willrut",
+            ssh_password="7TuB8binGL",
+            ssh_private_key="/ssh_key",
+            remote_bind_address=("basecamp-bigdata.informatik.uni-hamburg.de", 5432)) as server:
+
+        server.start()
+        print("Server connected.")
+
+        params = {
+            "user": "webcrawl2020",
+            "password": "BcN#DvUAgp",
+            "host": "basecamp-bigdata.informatik.uni-hamburg.de",
+            "port": 5432,
+            "database": "webcrawl"
+        }
+
+        connection = psycopg2.connect(**params)
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
+
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+'''finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")'''
