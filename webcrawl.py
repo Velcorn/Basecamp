fff637e1988698b0bdf50761a765bf7f7f0c12e0
velcorn <iwishyouperish@gmail.com>
2020-05-21 23:20:52 +0200

    Minor text fixes :P

--- webcrawl.py
+++ webcrawl.py
@@ -25,10 +25,12 @@ try:
         conn = connect(**params)
         cursor = conn.cursor()
         print("DB connected.")
+
         print(conn.get_dsn_parameters(), "\n")
         cursor.execute("SELECT version();")
         record = cursor.fetchone()
         print("You are connected to - ", record, "\n")
+
         cursor.close()
         conn.close()
         tunnel.close()
